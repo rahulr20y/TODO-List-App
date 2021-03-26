0d36ddb06130b92e5256e7430b7e471e41fec1ec
Rahul Yadav <rahulr20y@gmail.com>
2021-03-26 14:34:43 +0530

    creates DB Schema and linked it with list

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,7 @@
 
+const List = require('../models/list');
+
+
 
 //dummy Task List
 var arr = [
@@ -33,23 +36,49 @@ var arr = [
 
 module.exports.createTask = function(req,res){
     //console.log(req.body);
-    arr.push(req.body);
+    // arr.push(req.body);
 
-    return res.redirect('back');
+    // return res.redirect('back');
+
+
+
+    List.create({
+        description: req.body.description,
+        category: req.body.category,
+        dueDate: req.body.dueDate
+    },function(err,newContact){
+        if(err){
+            console.log('error in creating a contact');
+            return;
+        }
+
+        console.log('********',newContact);
+        return res.redirect('back');
+    });
+
 }
 
 
 module.exports.deleteTask = function(req,res){
 
-    console.log(req.query);
-    let description = req.query.description;
-    let taskIndex=arr.findIndex(task => task.description == description);
+    // console.log(req.query);
+    // let description = req.query.description;
+    // let taskIndex=arr.findIndex(task => task.description == description);
 
-    if(taskIndex != -1){
-        arr.splice(taskIndex,1);
-    }
+    // if(taskIndex != -1){
+    //     arr.splice(taskIndex,1);
+    // }
 
-    return res.redirect('back');
+    // return res.redirect('back');
+
+    let id=req.query.id;
+    List.findByIdAndDelete(id,function(err){
+        if(err){
+            console.log('error in deleting an object from database');
+            return;
+        }
+        return res.redirect('back');
+    });
 
 }
 
@@ -58,8 +87,24 @@ module.exports.home = function(req,res){
     //console.log(req);
     //res.send('Cool, it is running! or is it?');
 
-    return res.render('home',{
-        title: 'TODO List App',
-        tasks:arr
-    });
+    // return res.render('home',{
+    //     title: 'TODO List App',
+    //     //tasks:arr
+    //     tasks:List
+    // });
+
+
+
+        List.find({},function(err,list){
+                if(err){
+                    console.log('error int fetching the list from db');
+                    return;
+                }
+
+                return res.render('home',{
+                  title:"TODO List App",
+                  task_list: list
+                });
+
+        });
 };
