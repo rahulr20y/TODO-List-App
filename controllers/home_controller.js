480e614680c43ee0315573a2fb2492d14b8bfa75
Rahul Yadav <rahulr20y@gmail.com>
2021-03-26 08:50:21 +0530

    input form added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,5 +3,7 @@ module.exports.home = function(req,res){
     //console.log(req);
     //res.send('Cool, it is running! or is it?');
 
-    return res.render('home');
+    return res.render('home',{
+        title: 'TODO List App'
+    });
 };
